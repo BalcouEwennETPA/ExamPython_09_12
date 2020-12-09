dea726ffad0a0e442b5e989735d3a1b45326c201
BalcouEwennETPA <ewennbalcou10@gmail.com>
2020-12-09 15:27:04 +0100

    Update ExamPython_09_12.py

--- ExamPython_09_12.py
+++ ExamPython_09_12.py
@@ -8,8 +8,19 @@ mot=random.choice(listeMot)
 #print(random.choice(listeMot)) #choisir un mot random parmis la listeMot
 propMot = input ("faites une propositions de mots (six lettres max)")
 
-    for i in range (len(mot))
-        
-
-
-input()
+for i in range (len(mot)) :
+        if propMot == mot :
+            print (Fore.RED + mot)
+        elif propMot [0] != mot [0]
+            print (Fore.BLUE + mot)
+        elif propMot [1] != mot [1]
+            print (Fore.BLUE + mot)
+        elif propMot [2] != mot [2]
+            print (Fore.BLUE + mot)
+        elif propMot [3] != mot [3]
+            print (Fore.BLUE + mot)
+        elif propMot [4] != mot [4]
+            print (Fore.BLUE + mot)
+        elif propMot [5] != mot [5]
+            print (Fore.BLUE + mot)
+input ()
