df99e74706871878bacded99e2152966a650f8e7
BalcouEwennETPA <ewennbalcou10@gmail.com>
2020-12-09 15:11:56 +0100

    Update ExamPython_09_12.py

--- ExamPython_09_12.py
+++ ExamPython_09_12.py
@@ -5,11 +5,8 @@ from colorama import Fore, Back, Style
 
 listeMot = ["balcon" , "ballon" , "boules" , "suiver" , "viseur" , "abruti", "calque", "stereo" , "textes" , "argent"]
 mot=random.choice(listeMot)
-print(random.choice(listeMot)) #choisir un mot random parmis la listeMot
-
-    for i in range (0,9)
-        if i > 9
-        print ("vous avez perdu")
+#print(random.choice(listeMot)) #choisir un mot random parmis la listeMot
+propMot = input ("faites une propositions de mots (six lettres max)")
 
 
 input()
