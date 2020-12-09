60295d0cce931d522c8f9dd092faaf3d0aee8350
BalcouEwennETPA <ewennbalcou10@gmail.com>
2020-12-09 14:47:00 +0100

    Update ExamPython_09_12.py

--- ExamPython_09_12.py
+++ ExamPython_09_12.py
@@ -1,3 +1,10 @@
-listeMot = [ "balcon" , "ballon" , "boules" , "suiver" , "viseur" , "abruti", "calque", "stereo" , "textes" , "argent" ]
+import random
+from colorama import init
+init()
+from colorama import Fore, Back, Style
 
-print listeMot
+listeMot = ["balcon" , "ballon" , "boules" , "suiver" , "viseur" , "abruti", "calque", "stereo" , "textes" , "argent"]
+
+print (listeMot)
+
+input()
